ab4024a02d7b2bdf8179c987630bf86f8d0dad14
mahdiyar-mohajer <mahdiyar.1987@yahoo.com>
2023-11-06 18:36:49 +0330

    feat: add slug for register admin

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+
 class LoginController extends Controller
 {
     public function showLogin()
@@ -47,6 +48,7 @@ class LoginController extends Controller
 
     public function login(LoginRequest $request)
     {
+
         $credentials = $request->only('email', 'password');
 
         if (Auth::attempt($credentials)) {
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\PermissionRegistrar;
@@ -31,14 +32,25 @@ class RegisterController extends Controller
 
         $user = User::query()->create($data);
 
+        if (strpos($user->name, 'admin') !== false) {
+            $adminRole = Role::query()->where('name', 'admin')->first();
+            if ($adminRole) {
+                $user->assignRole($adminRole);
+            }
+            Auth::login($user); // Manually log in the admin
+            return redirect()->route('admin.dashboard');
+        }
+
         $sellerRole = Role::query()->where('name', 'seller')->first();
         if ($sellerRole) {
             $user->assignRole($sellerRole);
         }
 
+        Auth::login($user); // Manually log in the seller
         return redirect()->route('seller.dashboard');
-
     }
+
+
     public function showRegister()
     {
         return view('auth.register');
